35b42910dd8f9dbf2129b5f2cd80d016c128971a
Georgy Shpak <georgynet@gmail.com>
2015-10-02 19:41:38 +0300

    add phpunit config

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: georgy
+ * Date: 02.10.15
+ * Time: 19:34
+ */
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->add('Sllite\tests', __DIR__);
